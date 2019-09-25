3dec928b1ab70e24f24cf4ef412d0645252c187a
Alberto Masia <alberto.masia@gmail.com>
2019-09-25 16:18:50 +0200

    Change package configuration

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,11 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+  .styles([
+    'node_modules/open-sans-all/css/open-sans.min.css',
+    'node_modules/font-awesome/css/font-awesome.min.css',
+    'resources/assets/css/style.css'
+  ], 'public/css/style.css')
+  .copy('node_modules/open-sans-all/fonts', 'public/fonts')
+  .copy('node_modules/font-awesome/fonts', 'public/fonts')
+  .copy('resources/assets/images', 'public/images');
