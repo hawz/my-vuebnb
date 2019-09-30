23f61eb38e737904c0cbe229e8c4c4fd6492e905
hawz <alberto.masia@gmail.com>
2019-09-30 11:39:05 +0200

    Move main logic to single file component

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,29 +1,8 @@
 import Vue from 'vue';
-import { populateAmenitiesAndPrices } from './helpers'
-import ImageCarousel from './components/ImageCarousel';
-import ModalWindow from './components/ModalWindow';
-import HeaderImage from './components/HeaderImage';
-import FeatureList from './components/FeatureList';
-
-let model = window.vuebnb_listing_model;
-model = populateAmenitiesAndPrices(model);
+import ListingPage from './components/ListingPage';
 
 // My main vue instance, mounted on the div#app
 new Vue({
   el: '#app',
-  components: {
-    ImageCarousel,
-    ModalWindow,
-    HeaderImage,
-    FeatureList
-  },
-  data: {
-    ...model,
-    isContracted: true
-  },
-  methods: {
-    openModal() {
-      this.$refs.imagemodal.modalOpen = true;
-    }
-  }
+  render: h => h(ListingPage)
 });
---
+++ resources/assets/js/components/ExpandableText.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <p :class="{contracted: isContracted}">
+      <slot></slot>
+    </p>
+    <button class="more" @click="isContracted = !isContracted">
+      <span v-if="isContracted">+ More</span>
+      <span v-else>- Less</span>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isContracted: true
+    };
+  }
+};
+</script>
+
+<style>
+.about p {
+  white-space: pre-wrap;
+}
+
+.about p.contracted {
+  height: 250px;
+  overflow: hidden;
+}
+
+.about button.more {
+  background: transparent;
+  border: 0;
+  color: #008489;
+  padding: 0;
+  font-size: 17px;
+  font-weight: bold;
+}
+
+.about button.more:hover,
+.about button.more:active,
+.about button.more:focus {
+  text-decoration: underline;
+  outline: none;
+}
+</style>
