06e2ea7d4fb9c22ffa35519c93764c53c2cbaade
hawz <alberto.masia@gmail.com>
2019-09-30 12:23:30 +0200

    Create Router file

---
+++ resources/assets/js/router.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+import HomePage from './components/HomePage';
+import ListingPage from './components/ListingPage';
+
+Vue.use(VueRouter);
+
+export default new VueRouter({
+  mode: 'history',
+  routes: [
+    { path: '/', component: HomePage, name: 'home' },
+    { path: '/listing/:listing', component: ListingPage, name: 'listing' }
+  ]
+});
