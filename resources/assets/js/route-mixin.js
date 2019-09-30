1e0ffd4e7de446216a40627c46f0e4c62ab340aa
hawz <alberto.masia@gmail.com>
2019-09-30 18:31:53 +0200

    Move shared functionality between controllers to mixin

---
+++ resources/assets/js/route-mixin.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+function getData(to) {
+  return new Promise((resolve) => {
+    let serverData = window.vuebnb_server_data;
+    if (!serverData.path || to.path !== serverData.path) {
+      axios.get(`/api${to.path}`).then(({ data }) => {
+        resolve(data)
+      });
+    } else {
+      let serverData = window.vuebnb_server_data;
+      resolve(serverData);
+    }
+  })
+}
+
+export default {
+  beforeRouteEnter(to, from, next) {
+    getData(to).then(data => {
+      next(component => component.assignData(data));
+    });
+  }
+}
